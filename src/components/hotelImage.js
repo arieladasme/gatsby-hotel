74869b6bd3ebc8379059b376ed540c16ceae13a4
Ariel <ariel.wm1@gmail.com>
2020-08-04 20:57:27 -0400

    Adding css img background

--- src/components/hotelImage.js
+++ src/components/hotelImage.js
@@ -7,6 +7,34 @@ const BckgrndImg = styled(BackgroundImage)`
   height: 700px;
 `
 
+const TextImage = styled.div`
+  background-image: linear-gradient(
+    to top,
+    rgba(34, 49, 63, 0.8),
+    rgba(34, 49, 63, 0.8)
+  );
+  color: #fff;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    font-size: 4rem;
+    margin: 0%;
+    @media (min-width: 992px) {
+      font-size: 5.8rem;
+    }
+  }
+  p {
+    font-size: 2rem;
+    @media (min-width: 992px) {
+      font-size: 2.5rem;
+    }
+  }
+`
+
 const HotelImage = () => {
   const { image } = useStaticQuery(graphql`
     query {
@@ -20,14 +48,13 @@ const HotelImage = () => {
     }
   `)
 
-  //console.log(image.sharp.fluid)
-
   return (
-    <BckgrndImg
-      tag="section"
-      fluid={image.sharp.fluid}
-      fadeIn="soft"
-    ></BckgrndImg>
+    <BckgrndImg tag="section" fluid={image.sharp.fluid} fadeIn="soft">
+      <TextImage>
+        <h1>Bienvenido a Gatsby Hotel</h1>
+        <p>De choro</p>
+      </TextImage>
+    </BckgrndImg>
   )
 }
 
