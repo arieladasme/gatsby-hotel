2d2b470fa1c6d25c013e4618a3c19f7193360932
Ariel <ariel.wm1@gmail.com>
2020-08-04 20:09:09 -0400

    Adding CSS to Nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from '@emotion/styled'
+
+const Navigation = styled.nav`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 3rem;
+  @media (min-width: 768px) {
+    padding-bottom: 0;
+  }
+`
+
+const NavLink = styled(Link)`
+  color: #fff;
+  font-size: 1.6rem;
+  font-weight: 700;
+  line-height: 1;
+  font-family: 'PT Sans', sans-serif;
+  text-decoration: none;
+  padding: 1rem;
+  margin-right: 1rem;
+  &:last-of-type {
+    margin-right: 0;
+  }
+  &.selected {
+    border-bottom: 2px solid #fff;
+  }
+`
 
 const Nav = () => {
   return (
-    <nav>
-      <Link to="/">Inicio</Link>
-      <Link to="/us">Nosotros</Link>
-    </nav>
+    <Navigation>
+      <NavLink to="/" activeClassName="selected">
+        Inicio
+      </NavLink>
+      <NavLink to="/us" activeClassName="selected">
+        Nosotros
+      </NavLink>
+    </Navigation>
   )
 }
 
