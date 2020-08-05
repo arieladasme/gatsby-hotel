86aef90a3abf69ee84a650f72fd3958987734fa7
Ariel <ariel.wm1@gmail.com>
2020-08-05 13:47:46 -0400

    Css indexContent

--- src/components/indexContent.js
+++ src/components/indexContent.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Image from 'gatsby-image'
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
+
+const IndexText = styled.div`
+  padding-top: 4rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+  p {
+    line-height: 2;
+  }
+`
 
 const IndexContent = () => {
   const info = useStaticQuery(graphql`
@@ -23,11 +40,19 @@ const IndexContent = () => {
 
   return (
     <>
-      <h2>{title}</h2>
-      <div>
+      <h2
+        css={css`
+          text-align: center;
+          font-size: 4rem;
+          margin-top: 4rem;
+        `}
+      >
+        {title}
+      </h2>
+      <IndexText>
         <p>{content}</p>
         <Image fluid={image.fluid} />
-      </div>
+      </IndexText>
     </>
   )
 }
