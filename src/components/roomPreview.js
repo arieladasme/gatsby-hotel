d2f46f8036803aa572a6ae68ea31c84690c7d0da
Ariel <ariel.wm1@gmail.com>
2020-08-06 21:46:26 -0400

    Adding rooms preview

---
+++ src/components/roomPreview.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const RoomPreview = () => {
+  return <p>desde habitacion</p>
+}
+
+export default RoomPreview
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import { css } from '@emotion/core'
 import Layout from './../components/layout'
 import HotelImage from '../components/hotelImage'
 import IndexContent from '../components/indexContent'
+import RoomPreview from '../components/roomPreview'
+import useRooms from '../hooks/use-rooms'
 
-const IndexPage = () => (
-  <Layout>
-    <HotelImage />
-    <IndexContent />
-  </Layout>
-)
+const IndexPage = () => {
+  const rooms = useRooms()
+  console.log(rooms)
+  return (
+    <Layout>
+      <HotelImage />
+      <IndexContent />
+      <h2
+        css={css`
+          text-align: center;
+          margin-top: 5rem;
+          font-size: 3rem;
+        `}
+      >
+        Nuestas Habitaciones
+      </h2>
+
+      <ul>
+        {rooms.map(room => (
+          <RoomPreview />
+        ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export default IndexPage
